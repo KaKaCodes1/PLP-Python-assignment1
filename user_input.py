97a8b1a8f47dcaf6c25338f7d0b9d72151d0b881
KaKaCodes1 <139960971+KaKaCodes1@users.noreply.github.com>
2024-02-25 13:39:21 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name=input("Enter your name: ")
 age=int(input("Enter your age: "))
 location=input("Enter your location: ")
 
-print(f"Hello, {name}. You are {age} and live in {location}.")
+print(f"Hello, {name}. You are {age} years old and live in {location}.")
